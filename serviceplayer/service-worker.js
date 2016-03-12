bf29539b8f3607dfdf6d6ed2fb6d6e1729390dd8
mattdyer <madmatt1220@gmail.com>
2016-03-11 22:39:42 -0700

    service worker experimenting

--- serviceplayer/service-worker.js
+++ serviceplayer/service-worker.js
@@ -1,21 +1,9 @@
-/*
- Copyright 2015 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
 
-var OFFLINE_CACHE = 'offline';
-var FALLBACK_URL = 'notfound.html';
 
 var Options = {
-  OfflineURL:'offline.html'
+  OfflineURL:'offline.html',
+  OfflineCache:'offlineV1',
+  OfflineAvailablePages:[]
 };
 
 importScripts('serviceplayer/serviceworker-cache-polyfill.js');
@@ -30,16 +18,36 @@ self.addEventListener('message',function(event){
     }
   }
 
-  cacheOfflinePage(event);
-
+  cacheOfflinePage();
+  cachePages();
   console.log(event.data);
+
 });
 
 self.addEventListener('install',function(event) {
- cacheOfflinePage(event);
+ event.waitUntil(cacheOfflinePage());
+
+ event.waitUntil(cachePages());
+
 });
 
 self.addEventListener('fetch',function(event) {
+  /*
+  var response;
+    event.respondWith(caches.match(event.request).catch(function() {
+      return fetch(event.request);
+    }).then(function(r) {
+      response = r;
+      caches.open(Options.OfflineCache).then(function(cache) {
+        cache.put(event.request, response);
+      });
+      return response.clone();
+    }).catch(function() {
+      return caches.match(Options.OfflineURL);
+    }));
+*/
+
+
   // We only want to call event.respondWith() if this is a GET request for an HTML document.
   if (event.request.method === 'GET' &&
       event.request.headers.get('accept').indexOf('text/html') !== -1) {
@@ -59,7 +67,7 @@ self.addEventListener('fetch',function(event) {
         
         
        console.error('Fetch failed; returning offline page instead.', e);
-        return caches.open(OFFLINE_CACHE).then(function(cache) {
+        return caches.open(Options.OfflineCache).then(function(cache) {
           return cache.match(Options.OfflineURL);
         });
 
@@ -74,13 +82,17 @@ self.addEventListener('fetch',function(event) {
 });
 
 
-function cacheOfflinePage(event){
-   var offlineRequest = new Request(Options.OfflineURL);
-  event.waitUntil(
-    fetch(offlineRequest).then(function(response) {
-      return caches.open(OFFLINE_CACHE).then(function(cache) {
+function cacheOfflinePage(){
+  var offlineRequest = new Request(Options.OfflineURL);
+  fetch(offlineRequest).then(function(response) {
+      return caches.open(Options.OfflineCache).then(function(cache) {
         return cache.put(offlineRequest, response);
-      });
-    })
-  );
+    });
+  })
+}
+
+function cachePages(){
+  caches.open(Options.OfflineCache).then(function(cache) {
+    return cache.addAll(Options.OfflineAvailablePages);
+  });
 }
--- serviceplayer/serviceplayer.js
+++ serviceplayer/serviceplayer.js
@@ -1,30 +1,27 @@
 function servicePlayer(options){
-	sendMessage(options);
-}
+	
+  this.sendMessage = function(message) {
+    
+    return new Promise(function(resolve, reject) {
+      var messageChannel = new MessageChannel();
+      
+      messageChannel.port1.onmessage = function(event) {
+        if (event.data.error) {
+          reject(event.data.error);
+        } else {
+          resolve(event.data);
+        }
+      };
 
-function sendMessage(message) {
-  // This wraps the message posting/response in a promise, which will resolve if the response doesn't
-  // contain an error, and reject with the error if it does. If you'd prefer, it's possible to call
-  // controller.postMessage() and set up the onmessage handler independently of a promise, but this is
-  // a convenient wrapper.
-  return new Promise(function(resolve, reject) {
-    var messageChannel = new MessageChannel();
-    messageChannel.port1.onmessage = function(event) {
-      if (event.data.error) {
-        reject(event.data.error);
-      } else {
-        resolve(event.data);
+      if(navigator.serviceWorker.controller){
+        navigator.serviceWorker.controller.postMessage(message,[messageChannel.port2]);
       }
-    };
+    });
+  }
 
-    // This sends the message data as well as transferring messageChannel.port2 to the service worker.
-    // The service worker can then use the transferred port to reply via postMessage(), which
-    // will in turn trigger the onmessage handler on messageChannel.port1.
-    // See https://html.spec.whatwg.org/multipage/workers.html#dom-worker-postmessage
-    if(navigator.serviceWorker.controller){
-    	navigator.serviceWorker.controller.postMessage(message,[messageChannel.port2]);
-	}
-  });
+  this.sendMessage(options);
+
+  return this;
 }
 
 if ('serviceWorker' in navigator) {
